9531c531f757ccd9d1ae35585f95cdfda2f6ebdd
Draculoaf <2327163@students.wits.ac.za>
2022-09-13 12:05:16 +0200

    D3 local

--- d3.js
+++ d3.js
@@ -1,3 +1,6 @@
+console.log("hello", d3);
+
+//Data from https://www.popsugar.com/fitness/How-Much-Sugar-Soda-43838864
 const data = [
   { name: "Cocoa-Cola", sugar: 39 },
   { name: "Sprite", sugar: 38 },
@@ -7,14 +10,13 @@ const data = [
   { name: "7 Up", sugar: 38 },
 ];
 
-//Data from https://www.popsugar.com/fitness/How-Much-Sugar-Soda-43838864
-
 const width = 800;
 const height = 400;
 const margin = { top: 50, bottom: 50, left: 50, right: 50 };
 
-const svg = d3.select("#d3-container");
-.append('svg')
-.attr('height',height-margin.top-margin.bottom)
-.attr('width',width-margin.left-margin.right)
-.attr('viewBox',[0,0,width,height])
+const svg = d3
+  .select("#d3-container")
+  .append("svg")
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("width", width - margin.left - margin.right)
+  .attr("viewBox", [0, 0, width, height]);
